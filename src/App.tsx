819bbba2a559af7cd16dea1c818cfddb473da5c8
Linda1510 <niikolina.kircanski@gmail.com>
2023-08-15 19:55:43 +0200

    new

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+
+
+import Header from "./components/header";
+import "./styles/stles.scss";
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className="App">
+       <Header/>
+      </div>
+      
+     
+
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/header.tsx
@@ -0,0 +1,16 @@
+import Jungle from "./../assets/junglebackground.jpg";
+
+
+const Header =()=>{
+return <div className="header">
+ <img  src={Jungle} alt="background" />
+<div className="header__card">
+
+<h1>HELLO WORLD!</h1>
+ <h2>WELCOME TO MY PAGE!</h2>
+</div>
+
+
+    </div>
+};
+export default Header;
