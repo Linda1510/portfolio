50cd690b59a4fe007c0c0b2654985fddd86e8fc1
Linda1510 <niikolina.kircanski@gmail.com>
2023-09-03 00:49:31 +0200

    hamburger

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,19 +1,75 @@
+import { useState } from "react";
+
+
+
 const Header = () => {
+ 
+  const [isHamburgerOpen, setIsHamburgerOpen] = useState(false);
+
+  const toggleHamburger = () => {
+    setIsHamburgerOpen(!isHamburgerOpen);
+  };
+
+
+  const closeHamburger = () => {
+    setIsHamburgerOpen(false);
+  };
+
   return (
     <div className="header">
+      <div className="header__logo">Nikolina</div>
+      <div className={`header__link ${isHamburgerOpen ? "open" : ""}`}>
+        <a href="home" onClick={closeHamburger}>
+          Home
+        </a>
+        <a href="me" onClick={closeHamburger}>
+          Me
+        </a>
+        <a href="projects" onClick={closeHamburger}>
+          Projects
+        </a>
+        <a href="contact" onClick={closeHamburger}>
+          Contact
+        </a>
+        <a href="login" onClick={closeHamburger}>
+          Login
+        </a>
+        <a href="register" onClick={closeHamburger}>
+          Register
+        </a>
+      </div>
+      <div className="header__hamburger">
         <div className="header__logo">Nikolina</div>
-<div className="header__links">
-    
-            <a href="home">Home</a>
-            <a href="me">Me</a>
-            <a href="projects">Projects</a>
-            <a href="contact">Contact</a>
-            <a href="login">Login</a>
-            <a href="register">Register</a>
-            
-        
-</div>
+        <div className="header__hamburger__menu" onClick={toggleHamburger}>
+          <div className={`header__hamburger--icon ${isHamburgerOpen ? "open" : ""}`}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div className="header__hamburger--links">
+            <a href="home" onClick={closeHamburger}>
+              Home
+            </a>
+            <a href="me" onClick={closeHamburger}>
+              Me
+            </a>
+            <a href="projects" onClick={closeHamburger}>
+              Projects
+            </a>
+            <a href="contact" onClick={closeHamburger}>
+              Contact
+            </a>
+            <a href="login" onClick={closeHamburger}>
+              Login
+            </a>
+            <a href="register" onClick={closeHamburger}>
+              Register
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
+
 export default Header;
